cc69e4323a7adc0ed789c4c7c01dd588c7db19db
Francesco Aru <faru@codam.student.nl>
2023-05-15 13:08:10 +0200

    tmp

--- include/minishell.h
+++ include/minishell.h
@@ -1,5 +1,18 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define RED   "\x1B[31m"	// colors for the messages on the stdout
+# define GRN   "\x1B[32m"
+# define YEL   "\x1B[33m"
+# define BLU   "\x1B[34m"
+# define MAG   "\x1B[35m"
+# define RESET "\x1B[0m"
 # include "libft.h"
-# include "readline/readline.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>    	// malloc(), free()
+# include <unistd.h>    	// write(), read(), ...
+# include <fcntl.h>			// macro to open files (O_CREAT, O_WRONLY, O_RDONLY ..)
+# include <stdbool.h>		// boolean types
+# include <stdint.h>		// cross-compiler types
 #endif
--- sources/main/minishell.c
+++ sources/main/minishell.c
@@ -1,9 +1,41 @@
 #include "minishell.h"
 
+bool	add_cmd_to_hist(void)
+{
+	char *cmd;
+	bool next_cmd;
+	
+	cmd = readline("|-> ");
+	if (cmd && *cmd)
+		add_history(cmd);
+	else if (! ft_strncmp((const char *) cmd, "test", 4))
+	{
+		rl_replace_line("replaced!", 0);
+		rl_redisplay();
+	}
+	next_cmd = ft_strncmp((const char *) cmd, "end", 3);
+	free(cmd);
+	return (next_cmd);
+}
+
+void read_and_store(void)
+{
+	uint32_t i;
+
+	i = 0;
+	while (add_cmd_to_hist())
+		;
+	clear_history();
+}
+
+void l(void)
+{
+	system("leaks minishell");
+}
+
 int main(void)
 {
-    char *tmp2 = readline(">> ");
-    add_history(tmp2);
-    ft_printf("str read: %s\n", tmp2);
-    return (0);
+	// atexit(l);
+	read_and_store();
+	return (0);
 }
